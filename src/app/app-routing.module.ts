46f5a6c563ef9a2bdf94f400767971b2ede2b79d
trekkabout <robertstephens225@hotmail.com>
2021-03-19 14:06:32 -0500

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'locations',
-    pathMatch: 'full'
-  },
-  {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
-  },
-  {
-    path: 'locations',
-    loadChildren: () => import('./locations/locations.module').then( m => m.LocationsPageModule)
-  },
-  {
-    path: 'bookings',
-    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
-  },
+  { path: '', redirectTo: 'locations', pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
+  { path: 'locations', loadChildren: () => import('./locations/locations.module').then(m => m.LocationsPageModule) },
+  { path: 'bookings', loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule) }
+
 ];
 
 @NgModule({
--- src/app/locations/locations-routing.module.ts
+++ src/app/locations/locations-routing.module.ts
@@ -10,28 +10,35 @@ const routes: Routes = [
     children: [
         {
           path: 'offers',
-          loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule),
           children: [
             {
-              path: 'locationid',
+              path: '',
+              loadChildren: () => import('./offers/offers.module').then(m => m.OffersPageModule)
+            },
+            {
+              path: 'new',
               loadChildren: () => import('./offers/new/new.module').then( m => m.NewPageModule),
+            },
+            {
+              path: 'edit/:locationid',
+              loadChildren: () => import('./offers/edit/edit.module').then( m => m.EditPageModule),
             },
             {
               path: ':locationid',
               loadChildren: () => import('./offers/bookings/bookings.module').then( m => m.BookingsPageModule),
             },
-            {
-              path: 'offers/:locationid',
-              loadChildren: () => import('./offers/edit/edit.module').then( m => m.EditPageModule),
-            }
+
           ]
         },
         {
           path: 'discover',
-          loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule),
           children: [
             {
-              path: 'locationid',
+              path: '',
+              loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule)
+            },
+            {
+              path: ':locationId',
               loadChildren: () => import('./discover/details/details.module').then( m => m.DetailsPageModule),
             },
 
@@ -39,16 +46,69 @@ const routes: Routes = [
         },
         {
           path: '',
-          redirectTo: '.locations/tabs/discover',
+          redirectTo: '/locations/tabs/discover',
           pathMatch: 'full'
         }
     ],
   },
   {
     path: '',
-    redirectTo: '.locations/tabs/discover',
+    redirectTo: '/locations/tabs/discover',
     pathMatch: 'full'
   }
+
+
+
+  // {
+  //   path: 'tabs',
+  //   component: LocationsPage,
+  //   children: [
+  //     {
+  //       path: 'discover',
+  //       children: [
+  //         {
+  //           path: '',
+  //           loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule)
+  //         },
+  //         {
+  //           path: ':locationId',
+  //           loadChildren: () => import('./discover/details/details.module').then(m => m.DetailsPageModule)
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       path: 'offers',
+  //       children: [
+          // {
+          //   path: '',
+          //   loadChildren: () => import('./offers/offers.module').then(m => m.OffersPageModule)
+          // },
+  //         {
+  //           path: 'new',
+  //           loadChildren: () => import('./offers/new/new.module').then(m => m.NewPageModule)
+  //         },
+  //         {
+  //           path: 'edit/:locationId',
+  //           loadChildren: () => import('./offers/edit/edit.module').then(m => m.EditPageModule)
+  //         },
+  //         {
+  //           path: ':locationId',
+  //           loadChildren: () => import('./offers/bookings/bookings.module').then(m => m.BookingsPageModule)
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       path: '',
+  //       redirectTo: '/locations/tabs/discover',
+  //       pathMatch: 'full'
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: '/locations/tabs/discover',
+  //   pathMatch: 'full'
+  // }
 
 ];
 
