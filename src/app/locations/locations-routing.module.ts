2bd4d2591609ab61ac8583800a3d23d74bd8d37c
trekkabout <robertstephens225@hotmail.com>
2021-03-19 10:54:59 -0500

    tabs and routing adjustment

--- src/app/locations/locations-routing.module.ts
+++ src/app/locations/locations-routing.module.ts
@@ -5,17 +5,51 @@ import { LocationsPage } from './locations.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: LocationsPage,
+    children: [
+        {
+          path: 'offers',
+          loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule),
+          children: [
+            {
+              path: 'locationid',
+              loadChildren: () => import('./offers/new/new.module').then( m => m.NewPageModule),
+            },
+            {
+              path: ':locationid',
+              loadChildren: () => import('./offers/bookings/bookings.module').then( m => m.BookingsPageModule),
+            },
+            {
+              path: 'offers/:locationid',
+              loadChildren: () => import('./offers/edit/edit.module').then( m => m.EditPageModule),
+            }
+          ]
+        },
+        {
+          path: 'discover',
+          loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule),
+          children: [
+            {
+              path: 'locationid',
+              loadChildren: () => import('./discover/details/details.module').then( m => m.DetailsPageModule),
+            },
+
+          ]
+        },
+        {
+          path: '',
+          redirectTo: '.locations/tabs/discover',
+          pathMatch: 'full'
+        }
+    ],
+  },
+  {
     path: '',
-    component: LocationsPage
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+    redirectTo: '.locations/tabs/discover',
+    pathMatch: 'full'
   }
+
 ];
 
 @NgModule({
